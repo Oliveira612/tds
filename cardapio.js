be1a56bf7af08b9fd24580de924737ab39f6a637
Oliveira612 <oliveira.almeida.arthur@escola.pr.gov.br>
2026-05-06 12:29:38 +0000

    version4

--- cardapio.js
+++ cardapio.js
@@ -1,29 +1,50 @@
-cardapio
-
-
 const cardapio = [
-    {nome: "X-Burguer", preco: 18.90, categoria:"lanches"},
-    {nome: "Batata-frita", preco: 9.50, categoria:"acompanhamento"},
+    {nome: "X-Burguer", preco: 18.90, categoria: "lanche"},
+    {nome: "Batata-Frita", preco: 9.50, categoria: "acompanhamento"},
     {nome: "Suco de Laranja", preco: 7.00, categoria: "bebida"},
     {nome: "Milkshake", preco: 12.00, categoria: "sobremesa"}
 ];
 
-console.log(cardapio);
+//console.log(cardapio); //Impressão dos valores informados
 
-const pedido = [];
+const pedido = []; //lista vazia, vai receber os itens escolhidos
+
+const cliente=   {
+    nome: "Arthur", 
+    fidelidade: true
+}
 
 console.log(cardapio[0].nome);
-console.log(cardapio[1].preco); //preço de batata
+console.log(cardapio[1].preco); //preço da batata
 console.log(cardapio[2].categoria); //categoria do suco de laranja
 
 function exibirCardapio(lista) {
-    console.log("===Cardápio===");
+    console.log("===CARDAPIO===");
     console.log(`1. ${lista[0].nome} - R$ ${lista[0].preco}`)
     console.log(`2. ${lista[1].nome} - R$ ${lista[1].preco}`)
-    console.log(`3. $ ${lista[2].nome} - R$ $ {lista[2]preco}`)
+    console.log(`3. ${lista[2].nome} - R$ ${lista[2].preco}`)
+    console.log(`4. ${lista[3].nome} - R$ ${lista[3].preco}`)
 }
 
-function calcularTotal (listaPedido) {
-    let total= listaPedido[0].preco + listaPedido[1].preco;
+function calcularTotaL(listaPedido){
+    let total = listaPedido[0].preco + listaPedido[1].preco;
     return total;
 }
+
+function calcularDesconto(total, clienteObj) {
+    if (clienteObj.fidelidade === true) {
+        let desconto = total * 0.10;
+        console.log(`Desconto fidelidade: R${desconto}`);
+        return total - desconto;
+    }
+    return total;
+}
+
+function exibirPedido(listaPedido){
+    console.log("===SEU PEDIDO===");
+    console.log(`1. ${listaPedido[0].nome} - R$ ${listaPedido[0].preco}`);
+    console.log(`2. ${listaPedido[1].nome} - R$ ${listaPedido[1].preco}`);
+    console.log(`TOTAL: R$ ${calcularTotaL(listaPedido)}`);
+}
+
+exibirCardapio(cardapio);
