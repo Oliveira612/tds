f5e45f408f555989d5ef493c7e5644558c751243
Oliveira612 <oliveira.almeida.arthur@escola.pr.gov.br>
2026-05-12 11:52:50 +0000

    version4

---
+++ teste.js
@@ -0,0 +1,8 @@
+const destino = "Foz do Iguaçu";
+const origem = "Curitiba";
+const nomeViajante = "Arthur";
+let duracao = 8;
+let transporte = "Carro";
+
+//Declarar 5 varíaveis para cadastrar alunos
+
