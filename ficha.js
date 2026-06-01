a4d2523a9892e97e727dcd84e305bbe0c7f0959e
Oliveira612 <oliveira.almeida.arthur@escola.pr.gov.br>
2026-06-01 11:50:54 +0000

    version4

---
+++ ficha.js
@@ -0,0 +1,9 @@
+const animal = {
+    especie: "Cachorro", //string
+    idade: 4, // number
+    tutor: true, //boolean
+    vacinado: false//boolean
+};
+
+console.log(animal.nome);
+console.log(animal.tutor);
